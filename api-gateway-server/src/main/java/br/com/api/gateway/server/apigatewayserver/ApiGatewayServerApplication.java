d4011d4f90944558e271d1e379be69b38ea21167
tiago <si.tiagoferreira@gmail.com>
2018-04-22 22:09:33 -0300

    add api gateway server

---
+++ api-gateway-server/src/main/java/br/com/api/gateway/server/apigatewayserver/ApiGatewayServerApplication.java
@@ -0,0 +1,16 @@
+package br.com.api.gateway.server.apigatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ApiGatewayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayServerApplication.class, args);
+	}
+}
