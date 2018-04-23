6c96c5e84169acf7305886453bc2e3d14dbdfa18
tiago <si.tiagoferreira@gmail.com>
2018-04-22 21:37:58 -0300

    eureka naming server

---
+++ eureka-naming-server/src/main/java/br/com/eureka/naming/server/eurekanamingserver/EurekaNamingServerApplication.java
@@ -0,0 +1,14 @@
+package br.com.eureka.naming.server.eurekanamingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaNamingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaNamingServerApplication.class, args);
+	}
+}
