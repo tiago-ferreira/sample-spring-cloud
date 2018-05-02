27a9c446274c6e60e526883de9db9eed5206aef8
tiago <si.tiagoferreira@gmail.com>
2018-05-01 23:38:09 -0300

    creating a parent project and removed unused project

--- eureka-naming-server/src/main/java/br/com/eureka/naming/server/eurekanamingserver/EurekaNamingServerApplication.java
+++ eureka-naming-server/src/main/java/br/com/eureka/naming/server/eurekanamingserver/EurekaNamingServerApplication.java
@@ -1,8 +1,10 @@
 package br.com.eureka.naming.server.eurekanamingserver;
 
+import brave.sampler.Sampler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaServer
@@ -10,5 +12,10 @@ public class EurekaNamingServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaNamingServerApplication.class, args);
+	}
+
+	@Bean
+	public Sampler defaultSampler() {
+		return Sampler.ALWAYS_SAMPLE;
 	}
 }
