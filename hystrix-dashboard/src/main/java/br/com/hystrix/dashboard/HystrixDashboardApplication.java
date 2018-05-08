5c170482ce25749c3f925d348b54bcefb5b80652
tiago <si.tiagoferreira@gmail.com>
2018-05-07 23:15:38 -0300

    enable turbine

--- hystrix-dashboard/src/main/java/br/com/hystrix/dashboard/HystrixDashboardApplication.java
+++ hystrix-dashboard/src/main/java/br/com/hystrix/dashboard/HystrixDashboardApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 @EnableHystrixDashboard
+@EnableTurbine
 @EnableDiscoveryClient
 @SpringBootApplication
 public class HystrixDashboardApplication {
